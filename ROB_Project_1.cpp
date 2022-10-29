95212180ab45b1a340149d7349d204e34e29144a
tingweiouo <kklb1716@gmail.com>
2022-10-29 23:35:14 +0800

    remaining wrist angle solution

--- ROB_Project_1.cpp
+++ ROB_Project_1.cpp
@@ -1,16 +1,16 @@
 /*
 NYCU DME 0811070 TingWei Ou
 TODO:
-    1. input interface
+    1. input interface ok
     2. limit the operating region
 
         -160<θ1<160    -125<θ2<125
         -30<d3<30      -140<θ4<140
         -100<θ5<100    -260<θ6<260
 
-    3. use atan2 to calculate
+    3. use atan2 to calculate ok
     4.input: Cartesian point (n, o, a, p), output: the corresponding joint variables.
-    5.input: joint variables, output: Cartesian point (n, o, a, p) and (x, y, z, ϕ, θ, ψ).
+    5.input: joint variables, output: Cartesian point (n, o, a, p) and (x, y, z, ϕ, θ, ψ). ok
     
 */
 #include<iostream>
@@ -26,10 +26,11 @@ void CartesianPoint_output();
 
 //GLOBAL VARIABLE
 float PI = 3.1415926;
+float d2 = 6.375;
 char mode;
 float CartesianPoint[4]={0,0,0,0}; // [0] = n, [1] = o, [2] = a, [3] = p
 float joint_variables[6]={0,0,0,0,0,0}; // [0] = θ1, [1] = θ2, [2] = d3 , [3] = θ4, [4] = θ5, [5] = d6
-
+Eigen::Matrix<float, 4, 4> noap_input;
 
 //WHILE FLAG
 bool while_flag = false;
@@ -177,28 +178,44 @@ void joint_variables_mode_input(){
 //this function using global array CartesianPoint[i]
 void CartesianPoint_input(){
 
-    for(int i = 0; i < 4; i++){
-        cout<<"<-> please enter Cartesian Point [n, o, a, p] -> ";
+    cout<<"<-> please enter Cartesian Point [n, o, a, p] : "<<endl;
+    cout<<" | 00 , 01 , 02 , 03 |"<<endl;
+    cout<<" | 10 , 11 , 12 , 13 |"<<endl;
+    cout<<" | 20 , 21 , 22 , 23 |"<<endl;
+    cout<<" | 30 , 31 , 32 , 33 |"<<endl;
 
-        if( i == 0){
-            cout<<"n = ";
-            cin>>CartesianPoint[i];
-
-        }else if( i == 1){
-            cout<<"o = ";
-            cin>>CartesianPoint[i];
-
-        }else if( i == 2){
-            cout<<"a = ";
-            cin>>CartesianPoint[i];
-
-        }else if( i == 3){
-            cout<<"p = ";
-            cin>>CartesianPoint[i];
-
+    /*
+    for(int column = 0; column < 4; column++){
+        for(int row = 0; row < 4 ; row++){
+            cout<<" -> please enter "<<row<<column<<" : ";
+            cin>>noap_input(row, column);
         }
     }
+    */
+    noap_input(0, 0) = 0.3129;
+    noap_input(1, 0) = 0.7773;
+    noap_input(2, 0) = -0.5258;
+    noap_input(3, 0) = 0;
 
+    noap_input(0, 1) = -0.7773;
+    noap_input(1, 1) = 0.5398;
+    noap_input(2, 1) = 0.3231;
+    noap_input(3, 1) = 0;
+
+    noap_input(0, 2) = 0.5458;
+    noap_input(1, 2) = 0.3231;
+    noap_input(2, 2) = 0.7731;    
+    noap_input(3, 2) = 0;
+
+    noap_input(0, 3) = 4.2475;
+    noap_input(1, 3) = 8.3301;
+    noap_input(2, 3) = 18.7939;
+    noap_input(3, 3) = 1;
+
+    cout<<noap_input<<endl;
+
+    
+    //cout<<noap_input<<endl;
     cout<<"-----------------------------------"<<endl;
 }
 
@@ -275,5 +292,90 @@ void joint_variables_mode_output(){
 
 //this function handles inverse kinematics
 void CartesianPoint_output(){
+    float theta_1_1, theta_1_2, 
+          theta_2_1, theta_2_2, theta_2_3, theta_2_4,
+          theta_4,
+          theta_5,
+          theta_6,
+          d_3_1, d_3_2;
+    //8 solution by calculation
+
+    float JOINT_VARIABLE_SOLUTION_1[6] = {0,0,0,0,0,0};
+    float JOINT_VARIABLE_SOLUTION_2[6] = {0,0,0,0,0,0};
+    float JOINT_VARIABLE_SOLUTION_3[6] = {0,0,0,0,0,0};
+    float JOINT_VARIABLE_SOLUTION_4[6] = {0,0,0,0,0,0};
+    float JOINT_VARIABLE_SOLUTION_5[6] = {0,0,0,0,0,0};
+    float JOINT_VARIABLE_SOLUTION_6[6] = {0,0,0,0,0,0};
+    float JOINT_VARIABLE_SOLUTION_7[6] = {0,0,0,0,0,0};
+    float JOINT_VARIABLE_SOLUTION_8[6] = {0,0,0,0,0,0};
+
+    //dx = 03 ; dy = 13 ; dz = 23
+
+    //θ1 -> two solution
+    //θ1-1
+    theta_1_1 = atan2(noap_input(1, 3), noap_input(0, 3)) - atan2(d2, sqrt(pow(noap_input(0, 3), 2) + pow(noap_input(1, 3), 2) - pow(d2, 2)));
+    JOINT_VARIABLE_SOLUTION_5[0] = (theta_1_1*180/PI);
+    JOINT_VARIABLE_SOLUTION_6[0] = (theta_1_1*180/PI);
+    JOINT_VARIABLE_SOLUTION_7[0] = (theta_1_1*180/PI);
+    JOINT_VARIABLE_SOLUTION_8[0] = (theta_1_1*180/PI);
+
+    //θ1-2
+    theta_1_2 = (atan2(noap_input(1, 3), noap_input(0, 3)) - atan2(d2, -1*sqrt(pow(noap_input(0, 3), 2) + pow(noap_input(1, 3), 2) - pow(d2, 2))));
+    JOINT_VARIABLE_SOLUTION_1[0] = (theta_1_2*180/PI);
+    JOINT_VARIABLE_SOLUTION_2[0] = (theta_1_2*180/PI);
+    JOINT_VARIABLE_SOLUTION_3[0] = (theta_1_2*180/PI);
+    JOINT_VARIABLE_SOLUTION_4[0] = (theta_1_2*180/PI);
+
+    //θ2 -> four solution
+    //θ2-1
+    theta_2_1 = atan2((cos(theta_1_1)*noap_input(0, 3)) + (sin(theta_1_1)*noap_input(1, 3)), noap_input(2, 3));
+    JOINT_VARIABLE_SOLUTION_5[1] = (theta_2_1*180/PI);
+    JOINT_VARIABLE_SOLUTION_6[1] = (theta_2_1*180/PI);
+
+    //θ2-2
+    theta_2_2 = atan2((cos(theta_1_1)*noap_input(0, 3)) + (sin(theta_1_1)*noap_input(1, 3)), -1*noap_input(2, 3));
+    JOINT_VARIABLE_SOLUTION_3[1] = (theta_2_2*180/PI);
+    JOINT_VARIABLE_SOLUTION_4[1] = (theta_2_2*180/PI);
+
+    //θ2-3
+    theta_2_3 = atan2((cos(theta_1_2)*noap_input(0, 3)) + (sin(theta_1_2)*noap_input(1, 3)), noap_input(2, 3));
+    JOINT_VARIABLE_SOLUTION_1[1] = (theta_2_3*180/PI);
+    JOINT_VARIABLE_SOLUTION_2[1] = (theta_2_3*180/PI);
+
+    //θ2-4
+    theta_2_4 = atan2((cos(theta_1_2)*noap_input(0, 3)) + (sin(theta_1_2)*noap_input(1, 3)), -1*noap_input(2, 3));
+    JOINT_VARIABLE_SOLUTION_7[1] = (theta_2_4*180/PI);
+    JOINT_VARIABLE_SOLUTION_8[1] = (theta_2_4*180/PI);
+
+    //d3 -> two solution
+    //d3-1
+    d_3_1 = sin(theta_1_1)*cos(theta_1_1)*noap_input(0, 3) + sin(theta_1_1)*sin(theta_2_1)*noap_input(1, 3) + cos(theta_2_1)*noap_input(2, 3);
+    JOINT_VARIABLE_SOLUTION_1[2] = d_3_1;
+    JOINT_VARIABLE_SOLUTION_2[2] = d_3_1;
+    JOINT_VARIABLE_SOLUTION_5[2] = d_3_1;
+    JOINT_VARIABLE_SOLUTION_6[2] = d_3_1;
+
+    //d3-2
+    d_3_2 = d_3_1*-1;
+    JOINT_VARIABLE_SOLUTION_3[2] = d_3_1;
+    JOINT_VARIABLE_SOLUTION_4[2] = d_3_1;
+    JOINT_VARIABLE_SOLUTION_7[2] = d_3_1;
+    JOINT_VARIABLE_SOLUTION_8[2] = d_3_1;
+
+    //θ4
+
+
+    //θ5
+
+
+    //θ6
+
+    //print solution 1
+    cout<<"<sol_1>Corresponding variables (θ1, θ2, d3, θ4, θ5, θ6): "<<endl;
+    for(int i = 0; i < 6; i++){
+        cout<<JOINT_VARIABLE_SOLUTION_1[i]<<" ";
+    }
+    cout<<endl;
+    
 
 }
