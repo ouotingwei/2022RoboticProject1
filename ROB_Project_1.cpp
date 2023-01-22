315834bb105562c5f76e9e4033f325f29560095f
TingWei Ou <87459096+ouotingwei@users.noreply.github.com>
2023-01-23 00:18:56 +0800

    更新 ROB_Project_1.cpp

--- ROB_Project_1.cpp
+++ ROB_Project_1.cpp
@@ -1,18 +1,5 @@
 /*
 NYCU DME 0811070 TingWei Ou
-TODO:
-    1. input interface ok
-    2. limit the operating region
-
-        -160<θ1<160    -125<θ2<125
-        -30<d3<30      -140<θ4<140
-        -100<θ5<100    -260<θ6<260
-
-    3. use atan2 to calculate ok
-    4.input: Cartesian point (n, o, a, p), output: the corresponding joint variables.
-    5.input: joint variables, output: Cartesian point (n, o, a, p) and (x, y, z, ϕ, θ, ψ). ok
-    6.inverse kinamatics error correction (asin & float)
-    
 */
 #include<iostream>
 #include<Eigen/Dense>
